04dc69ae0b28b9416532dcb0f9eec36d63a22a92
Jenny Allar <JennyAllar@gmail.com>
2014-07-29 17:50:06 -0600

    Testing if string has an exit.

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -2,4 +2,8 @@ class Maze < ActiveRecord::Base
   def has_entrance?(string)
     string.include?("E")
   end
+
+  def has_exit?(string)
+    string.include?("X")
+  end
 end
--- spec/models/mazes_spec.rb
+++ spec/models/mazes_spec.rb
@@ -3,10 +3,16 @@ require 'rails_helper'
 describe Maze do
   it 'can determine if there is an entrance in the room' do
     input = "[E**|] [|***] [|*|*] [|***] [||**]"
-    
     actual = Maze.new.has_entrance?(input)
     
     expect(actual).to eq true
- 
   end
-end
+
+  it 'can determine if there is an exit in the room' do
+    input = "[*||E] [|**|] [*|**] [*|||] [|X*|]"
+    actual = Maze.new.has_exit?(input)
+
+    expect(actual).to eq true
+  end
+
+  end
